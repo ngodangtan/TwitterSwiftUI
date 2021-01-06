118d71ee25802054f95803cb6a4d2893f1c90ff7
Ngo Dang Tan <ngodangtan1994@gmail.com>
2021-01-06 11:24:34 +0700

    first commit

---
+++ TwitterSwiftUI/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUI
+//
+//  Created by Ngo Dang tan on 06/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                Text("Search View")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Home")
+                    }
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+             
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Ngo Dang tan on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
