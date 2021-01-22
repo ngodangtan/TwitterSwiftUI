7938e10255fe4782a9507352603ef57710f6ac7a
Ngo Dang Tan <ngodangtan1994@gmail.com>
2021-01-22 15:30:39 +0700

    add firebase swift package manager

--- TwitterSwiftUI/TwitterSwiftUIApp.swift
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct TwitterSwiftUIApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             //ContentView()
--- TwitterSwiftUI/View/Authentication/RegistrationView.swift
+++ TwitterSwiftUI/View/Authentication/RegistrationView.swift
@@ -12,18 +12,48 @@ struct RegistrationView: View {
     @State var password = ""
     @State var fullname = ""
     @State var username = ""
+    @State var showImagePicker = false
+    @State var selectedUIImage: UIImage?
+    @State var image: Image?
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
+    func loadImage(){
+        guard let selectedImage = selectedUIImage else {return}
+        image = Image(uiImage: selectedImage)
+    }
     var body: some View {
         ZStack {
             VStack {
-                Image("plus_photo")
-                    .resizable()
-                    .renderingMode(.template)
-                    .scaledToFill()
-                    .frame(width: 140, height: 140)
-                    .padding(.top,88)
-                    .padding(.bottom,16)
-                    .foregroundColor(.white)
+                Button(action: { showImagePicker.toggle() }, label: {
+                    ZStack {
+                        if let image = image {
+                            image
+                                .resizable()
+                             
+                                .scaledToFill()
+                                .frame(width: 140, height: 140)
+                                .clipped()
+                                .cornerRadius(70)
+                                .padding(.top,88)
+                                .padding(.bottom,16)
+                             
+                            
+                        }else{
+                            Image("plus_photo")
+                                .resizable()
+                                .renderingMode(.template)
+                                .scaledToFill()
+                                .frame(width: 140, height: 140)
+                                .padding(.top,88)
+                                .padding(.bottom,16)
+                                .foregroundColor(.white)
+                        }
+                 
+                    }
+                }).sheet(isPresented: $showImagePicker, onDismiss: loadImage, content: {
+                    ImagePicker(image: $selectedUIImage)
+                })
+             
                 
                 VStack(spacing: 20) {
                     CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelop")
